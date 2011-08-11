6c1173280ca1ee58d9e2f37b1c9f609bf3006585
Jason Gross <jgross@mit.edu>
2011-08-11 00:03:25 -0400

    Created version variable.

--- update-version.sh
+++ update-version.sh
@@ -2,7 +2,8 @@
 BASE="`git rev-parse --show-toplevel`"
 chmod +w "$BASE/VERSION" "$BASE/cache.manifest"
 echo VERSION:
-"$BASE/version.sh" | tee "$BASE/VERSION"
+echo -n $("$BASE/version.sh") | tee "$BASE/VERSION"
+echo
 echo
 echo cache.manifest:
 sed s"/^# version.*$/# version $("$BASE/version.sh")/i" "$BASE/cache.manifest.in" > "$BASE/cache.manifest"
