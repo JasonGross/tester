d4adb19c440a9886599672a173258e34fbc96a45
Jason Gross <jgross@mit.edu>
2011-08-30 03:30:20 -0400

    First pass of persistant state.  Has bugs.

--- scripts/javascript/tester.js
+++ scripts/javascript/tester.js
@@ -13,42 +13,78 @@ var Tester;
     this.onNewRound = function newRoundEvent(depth, count) {};
     this.onFinish = function onFinish() {};
 
-    function doTestingRound(tests, depth, callback) {
-      var nextTests;
-      self.onNewRound(depth, tests.length);
-      nextTests = [];
-
-      function doNextTest(testsLeft) {
-        if (testsLeft.length > 0) {
-          var test = testsLeft.pop();
-          self.ask(test[0], function gotResponse(given) {
-            if (self.isCorrect(test[1], given)) {
-              self.onCorrect(test[0], test[1], given);
-            } else {
-              self.onIncorrect(test[0], test[1], given);
-              nextTests.push(test);
-            }
-            doNextTest(testsLeft);
-          });
-        } else {
-          if (nextTests.length > 0) {
-            doTestingRound(nextTests, depth + 1, function () {
-              doTestingRound(tests, depth, callback);
-            });
-          } else {
-            if (callback)
-              callback();
-            else
-              self.onFinish();
-          }
-        }
+    // restores the state.  Returns false on success, or the error on failure
+    this.restoreState = function restoreState() {
+      try {
+        var state = JSON.parse(localStorage['current state']);
+        triggerOnNewRound(state);
+        doNextTest(state);
+        return false;
+      } catch (ex) {
+        return ex;
       }
+    };
 
-      doNextTest(sample(tests)); // order randomly
+    this.clearState = function clearState() {
+      localStorage['current state'] = undefined;
     }
 
-    this.beginTesting = function beginTesting() {
-      doTestingRound(tests, 0);
+    function doNextTest(state) {
+      localStorage['current state'] = JSON.stringify(state); // save state
+
+      var test = state['current round left'].pop();
+      console.log(JSON.stringify(test));
+      console.log(JSON.stringify(state));
+      if (test !== undefined) {
+        self.ask(test[0], function gotResponse(given) {
+          if (self.isCorrect(test[1], given)) {
+            self.onCorrect(test[0], test[1], given);
+          } else {
+            self.onIncorrect(test[0], test[1], given);
+            // note that the user got this test wrong, for the next round
+            state['indices wrong'][0].push(test);
+          }
+          doNextTest(state);
+        });
+      } else {
+        if (state['indices wrong'][0].length == 0) {
+          // if the user gets nothing wrong, remove the empty list of things wrong,
+          // and the list of things prompted in this round
+          state['indices wrong'].shift();
+          state['indices wrong'].shift();
+        }
+        doNextTestingRound(state);
+      }
+    }
+
+    function hasMoreTestingRounds(state) {
+      return state['indices wrong'].length > 0;
+    }
+
+    function triggerOnNewRound(state) {
+      var rounds = state['indices wrong'];
+      self.onNewRound(rounds.length - 2, rounds[1].length); // 0 based index
+    }
+
+    function doNextTestingRound(state) {
+      if (hasMoreTestingRounds(state)) {
+        var rounds = state['indices wrong'];
+        state['current round left'] = sample(rounds[0]);
+        state['indices wrong'].unshift([]);
+        triggerOnNewRound(state);
+        doNextTest(state);
+      } else {
+        self.clearState();
+        self.onFinish();
+      }
+    }
+
+    this.beginTesting = function beginTesting(tryRestore) {
+      if (tryRestore === undefined) tryRestore = true;
+      if (!tryRestore || self.restoreState()) { // self.restoreState() is true if there was an exception
+        var state = {'tests':tests, 'current round left':undefined, 'indices wrong':[sample(tests)]};
+        doNextTestingRound(state);
+      }
     };
 
     return this;
