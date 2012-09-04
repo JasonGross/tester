1ea8ba97b701d943382cb76e5d0d92366b3bf0f0
Jason Gross <jgross@mit.edu>
2012-09-04 17:30:45 -0400

    Bugfixes
    * Fix jQuery version in list-editor
    * Factor out common code in tester, list-editor
    * Time-stamp cache.manifest so whenever we remake it, we get updates.
    * Don't show "cache update error" if you're offline.

--- update-version.sh
+++ update-version.sh
@@ -7,6 +7,8 @@ echo
 echo
 echo cache.manifest:
 sed s"/^# version.*$/# version $("$BASE/version.sh")/i" "$BASE/cache.manifest.in" > "$BASE/cache.manifest"
+echo >> "$BASE/cache.manifest"
+echo "# Time Stamp: `date +%s`" >> "$BASE/cache.manifest"
 cat "$BASE/cache.manifest"
 chmod -w "$BASE/VERSION" "$BASE/cache.manifest"
 # create google analytics file if it doesn't exist
